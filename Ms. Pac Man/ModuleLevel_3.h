de6373343cd3b20a1ab20b29c3aa67d1af162e2e
AlfonsoSanchez <alfonsosanchez1896@gmail.com>
2016-05-16 11:20:30 +0200

    Add module level 3 and Tile

---
+++ Ms. Pac Man/ModuleLevel_3.h
@@ -0,0 +1,36 @@
+#ifndef __MODULELEVEL3_H__
+#define __MODULELEVEL3_H__
+
+#include "Module.h"
+#include "Animation.h"
+#include "Globals.h"
+
+struct SDL_Texture;
+
+class ModuleLevel3 : public Module
+{
+public:
+	ModuleLevel3();
+	~ModuleLevel3();
+
+	bool Start();
+	update_status Update();
+	bool CleanUp();
+
+public:
+	Animation big_pill;
+	Animation* actual_animation = &big_pill;
+	SDL_Texture* graphics = nullptr;
+	SDL_Texture* graphics_2 = nullptr;
+	SDL_Rect level3;
+	SDL_Rect level3_2;
+	SDL_Rect level3_center;
+	SDL_Rect pills;
+	Animation water;
+
+	int map[31][28];
+	int eatenpills;
+	bool victory;
+};
+
+#endif
